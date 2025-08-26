0b4690024ea43d1d2fe630d6b46d0e334f2098af
awajge <akshat.wajge@gmail.com>
2025-08-25 20:51:39 -0500

    nc

--- internship_notifier.py
+++ internship_notifier.py
@@ -110,7 +110,7 @@ def add_internships(link):
 def format(data):
     link_sub = truncate(data[0], 50, False).strip()
     line = (f'<a href="{data[5]}" target="_blank">{link_sub}</a>') + (' ' * (50 + (GAP//2) - len(link_sub)) + '|' + ' ' * (GAP//2)) # clickable position title
-    line += truncate(data[1], 15) # span - disable making unwanted clickable links
+    line += truncate(data[1], 25)
     line += truncate(data[2], 10)
     line += truncate(data[3], 16)
     line += truncate(", ".join(str(tag) for tag in data[4]), 33, False).strip()
@@ -144,7 +144,7 @@ for link_data in internships.keys():
     for data in internships[link_data]:
         message_text += format(data) + "\n"
 
-message = MIMEText(f'<font face="monospace" size="2"><pre>{message_text}</pre></font>', 'html')
+message = MIMEText(f'<pre style="font-family: monospace;">{message_text}</pre>', 'html')
 
 message['Subject'] = f"Intern Bot: {sum(map(len, internships.values()))} internships found on {strftime("%Y-%m-%d", localtime(time()))}"
 message["From"] = USERNAME
